11be4542de36f1aa578dc2bd27c23d67d7869c83
mihakremen <69080408+mihakremen@users.noreply.github.com>
2021-06-05 10:27:42 +0500

    solving the problem of CLIENT instead of SERVER

--- scp_pysx127x/pySX127x/mqtt_LORA_SERVER.py
+++ scp_pysx127x/pySX127x/mqtt_LORA_SERVER.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" This program sends a response whenever it receives the "INF" """
+""" This program asks a client for data and waits for the response, then sends an ACK. """
 
 # Copyright 2018 Rui Silva.
 #
@@ -23,14 +23,15 @@
 
 import time
 from SX127x.LoRa import *
-#from SX127x.LoRaArgumentParser import LoRaArgumentParser
+# from SX127x.LoRaArgumentParser import LoRaArgumentParser
 from SX127x.board_config import BOARD
 import mosquitto
 
-
 BOARD.setup()
 BOARD.reset()
-#parser = LoRaArgumentParser("Lora tester")
+
+
+# parser = LoRaArgumentParser("Lora tester")
 
 
 class mylora(LoRa):
@@ -38,27 +39,23 @@ class mylora(LoRa):
         super(mylora, self).__init__(verbose)
         self.set_mode(MODE.SLEEP)
         self.set_dio_mapping([0] * 6)
+        self.var = 0
 
     def on_rx_done(self):
         BOARD.led_on()
-        #print("\nRxDone")
+        # print("\nRxDone")
         self.clear_irq_flags(RxDone=1)
-        payload = self.read_payload(nocheck=True )# Receive INF
+        payload = self.read_payload(nocheck=True)
         print("Receive: ")
-        mens = bytes(payload).decode("utf-8",'ignore')
-        mens = mens[2:-1] #to discard \x00\x00 and \x00 at the end
-        print(mens)
-        mosquitto.send_mqtt("safetyDacha/Device_1", mens)
+        res_data = (bytes(payload).decode("utf-8", 'ignore'))
+        print(res_data)  # Received DATA
+        mosquitto.send_mqtt("safetyDacha/Device_1", res_data)
         BOARD.led_off()
-        if mens == "INF":
-            print("Received data request INF")
-            time.sleep(2)
-            print("Send mens: DATA RASPBERRY PI")
-            self.write_payload([255, 255, 0, 0, 68, 65, 84, 65, 32, 82, 65, 83, 80, 66, 69, 82, 82, 89, 32, 80, 73, 0]) # Send DATA RASPBERRY PI
-            self.set_mode(MODE.TX)
-        time.sleep(2)
-        self.reset_ptr_rx()
-        self.set_mode(MODE.RXCONT)
+        time.sleep(1)  # Wait for the client be ready
+        #        print ("Send: ACK")
+        #        self.write_payload([255, 255, 0, 0, 65, 67, 75, 0]) # Send ACK
+        self.set_mode(MODE.TX)
+        self.var = 1
 
     def on_tx_done(self):
         print("\nTxDone")
@@ -84,33 +81,45 @@ class mylora(LoRa):
         print("\non_FhssChangeChannel")
         print(self.get_irq_flags())
 
-    def start(self):          
+    def start(self):
         while True:
+            while (self.var == 0):
+                print("Send: INF")
+                self.write_payload([255, 255, 0, 0, 73, 78, 70, 0])  # Send INF
+                self.set_mode(MODE.TX)
+                time.sleep(3)  # there must be a better solution but sleep() works
+                self.reset_ptr_rx()
+                self.set_mode(MODE.RXCONT)  # Receiver mode
+
+                start_time = time.time()
+                while (time.time() - start_time < 10):  # wait until receive data or 10s
+                    pass;
+
+            self.var = 0
             self.reset_ptr_rx()
-            self.set_mode(MODE.RXCONT) # Receiver mode
-            while True:
-                pass;
-            
+            self.set_mode(MODE.RXCONT)  # Receiver mode
+            time.sleep(10)
+
 
 lora = mylora(verbose=False)
-#args = parser.parse_args(lora) # configs in LoRaArgumentParser.py
+# args = parser.parse_args(lora) # configs in LoRaArgumentParser.py
 
 #     Slow+long range  Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. 13 dBm
 lora.set_pa_config(pa_select=1, max_power=21, output_power=15)
 lora.set_bw(BW.BW125)
 lora.set_coding_rate(CODING_RATE.CR4_8)
 lora.set_spreading_factor(12)
-lora.set_rx_crc(True)
-#lora.set_lna_gain(GAIN.G1)
-#lora.set_implicit_header_mode(False)
+lora.set_rx_crc(False)
+# lora.set_lna_gain(GAIN.G1)
+# lora.set_implicit_header_mode(False)
 lora.set_low_data_rate_optim(True)
+lora.set_freq(868)
 
 #  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
-#lora.set_pa_config(pa_select=1)
+# lora.set_pa_config(pa_select=1)
 
 
-
-assert(lora.get_agc_auto_on() == 1)
+assert (lora.get_agc_auto_on() == 1)
 
 try:
     print("START")
@@ -123,4 +132,4 @@ finally:
     sys.stdout.flush()
     print("Exit")
     lora.set_mode(MODE.SLEEP)
-BOARD.teardown()
+    BOARD.teardown()
